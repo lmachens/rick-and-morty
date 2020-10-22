38ae17309091d97c6f2b8b0a3c8de0e5eeb9e380
Leon Machens <leon.machens@googlemail.com>
2020-10-22 16:30:56 +0200

    ✨⚡ Add infinite loader (#4)
    * Add infinite loader
    
    * Reappend loadMoreButton to avoid scrolling
    
    * Load more on scroll
    
    * Add lazy load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./app.css";
+import Button from "./components/Button";
 import Character from "./components/Character";
 import Characters from "./components/Characters";
 import Header from "./components/Header";
@@ -7,34 +8,60 @@ import { getCharacters } from "./utils/api";
 import { createElement } from "./utils/elements";
 
 function App() {
+  let lastName = null;
+  let nextPage = null;
+
   const header = Header();
 
   const characterContainer = Characters();
-  const main = createElement("main", {
-    className: "main",
-    children: [characterContainer],
+  const loadMoreButton = Button({
+    innerText: "Load more",
+    onclick: () => {
+      loadCharacters(lastName, nextPage);
+    },
   });
 
-  async function loadCharacters(name) {
-    const characters = await getCharacters(name);
-    const characterElements = characters.map((character) =>
+  const main = createElement("main", {
+    className: "main",
+    children: [characterContainer, loadMoreButton],
+  });
+
+  async function loadCharacters(name, page) {
+    const characters = await getCharacters(name, page);
+    const characterElements = characters.results.map((character) =>
       Character({
         name: character.name,
         imgSrc: character.image,
       })
     );
-    characterContainer.innerHTML = "";
     characterContainer.append(...characterElements);
+
+    nextPage = characters.info.next?.match(/\d+/)[0];
+    loadMoreButton.disabled = !characters.info.next;
+    lastName = name;
+    // Reappend loadMoreButton to avoid scrolling
+    main.append(loadMoreButton);
   }
 
   const search = Search({
-    onchange: (value) => loadCharacters(value),
+    onchange: (value) => {
+      characterContainer.innerHTML = "";
+      loadCharacters(value);
+    },
   });
 
   loadCharacters();
 
   const container = createElement("div", {
     children: [header, search, main],
+  });
+
+  window.addEventListener("scroll", () => {
+    const offsetY =
+      loadMoreButton.offsetParent.offsetHeight - window.innerHeight - 200;
+    if (offsetY < window.pageYOffset) {
+      loadMoreButton.click();
+    }
   });
   return container;
 }
